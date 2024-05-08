f051962206369f4e1f0325d4b758674bd745732d
Marco Annunziata <marco.annunziata@edu.unife.it>
2024-05-08 09:40:26 +0200

    Prova preliminare di mappatura oggetti del DB

--- src/components/BackendInteract.vue
+++ src/components/BackendInteract.vue
@@ -1,83 +1,109 @@
 <script setup lang="ts">
-import axios from 'axios';
-import { ref } from 'vue';
+  import axios from 'axios';
+  import type { AxiosResponse } from 'axios';
+  import { ref } from 'vue';
 
-// In teoria gli attributi devono essere dello stesso nome e tipo e nello stesso ordine di come vengono forniti dal backend
+  const data = ref([]);
+  const dataVenue = ref<AllowedData>([]);
+  const dataPersonnel = ref([]);
+  type AllowedData = Venue[];
 
-class Luogo{
-  "id": number;
-  "address": string;
-  "closure_date": string;
-  "holiday_start_schedule": string;
-  "holiday_end_schedule": string;
-  "weekday_start_schedule": string;
-  "weekday_end_schedule": string;
-  "capacity": number;
-  "hourly_rent_cost": number;
-}
-class Personale{
-  "name": string;
-  "sector": string;
-  "hourly_prenotation_cost": number;
-}
-class Prenotazione{
-  "id": number;
-  "date": string;
-  "time": string;
-  luogo: Luogo;
-  personale: Personale;
-  constructor(id: number, date: string, time: string, luogo: Luogo, personale: Personale) {
-    this.id = id;
-    this.date = date;
-    this.time = time;
-    this.luogo = new Luogo(); //TODO da sistemare
-    this.personale = new Personale();
+  interface Venue{
+    id: number;
+    name: string;
+    address: string;
+    max_capacity: number;
+    rent_cost: number;
+    weekdayHours: {
+      open: string;
+      close: string
+    };
+    weekendHours: {
+      open: string;
+      close: string
+    };
+    closingDays: string[];
+    booking: Booking;
   }
-}
 
-class Prova{
-  "id": number;
-  "name": string;
-  "address": string;
-}
+  interface Personnel{
+    name: string;
+    sector: string;
+    booking_cost: number;
+  }
 
-//const data = ref([]);
-//const data = ref(0);
-//const data = ref<MyTable[]>([]);
-const data = ref<Prova[] | Luogo[] | Personale[] | Prenotazione[]>([]);
+  interface Booking{
+    id: number;
+    date: string;
+    time: string;
+    venue: Venue;
+    Personnel: Personnel;
+  }
 
-axios.get("/api/database/ls").then(response => {
-  console.log("Risposta da Axios:", response);
-  console.log("Dati ricevuti:", response.data);
-  data.value = response.data;
-}).catch(error => {
-  console.error("Errore durante la richiesta Axios:", error);
-});
+  class Prova{
+    id: number;
+    name: string;
+    address: string;
+
+    constructor(id: number, name: string, address: string) {
+      this.id = id;
+      this.name = name;
+      this.address = address;
+    }
+  }
+
+  function creaVenue(item: Venue){
+    const newVenue = new Prova(item.id, item.name, item.address);
+    console.log(newVenue.id, newVenue.name, newVenue.address);
+    console.log(item instanceof Prova);
+  }
+
+  /*axios.get("/api/database/ls").then(response => {
+    console.log("Risposta da Axios:", response);
+    console.log("Dati ricevuti:", response.data);
+    data.value = response.data;
+  }).catch(error => {
+    console.error("Errore durante la richiesta Axios:", error);
+  });*/
+
+  axios.get<AllowedData>("/api/database/ls").then((response: AxiosResponse<AllowedData>) => {
+    console.log("Risposta da Axios:", response);
+    console.log("Dati ricevuti:", response.data);
+    if(response.data.every(item => 'id' in item)) {
+      dataVenue.value = response.data
+      console.log("Venue", dataVenue.value);
+    }
+    else if(response.data.every(item => 'sector' in item)) {
+      console.log("AuxiliaryPersonnel", response.data);
+    }
+  }).catch(error => {
+    console.error("Errore durante la richiesta Axios:", error);
+  });
+
 </script>
 
 <template>
-  <div>
-    <table>
-      <tr v-for="items in data" :key="items.id" >
-        <td>{{ items.id }}</td>
-        <td>{{ items.name }}</td>
-        <td>{{ items.address }}</td>
-      </tr>
-    </table>
-    <div v-if="data.length > 0">
-      Numero di elementi in data: {{ data.length }}
-    </div>
+  <ul v-if="dataVenue.length > 0">
+    <li v-for="(item, index) in dataVenue" :key="index">
+      Item {{ index }}: <br/>
+      {{ item}}
+      <button @click="creaVenue(item)">Crea Venue</button>
+      <hr>
+    </li>
+  </ul>
+  <div v-else>
+    Errore: nessun elemento letto dalla richiesta HTTP <!-- non ci sarebbe bisogno perchè già in console spunta l'errore -->
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-h3 {
-  font-size: 1.2em;
-}
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+  }
+  h3 {
+    font-size: 1.2em;
+  }
 </style>
